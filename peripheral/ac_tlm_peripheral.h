e1e7cb4b219e6f38f6b8837da082b5fd92a39bb0
Renan Gomes Pereira <ra103927@stones.lab.ic.unicamp.br>
2016-06-17 10:03:44 -0300

    starting peripherals

---
+++ peripheral/ac_tlm_peripheral.h
@@ -0,0 +1,162 @@
+/**:
+ * @file      ac_tlm_mem.h
+ * @author    Bruno de Carvalho Albertini
+ *
+ * @author    The ArchC Team
+ *            http://www.archc.org/
+ *
+ *            Computer Systems Laboratory (LSC)
+ *            IC-UNICAMP
+ *            http://www.lsc.ic.unicamp.br/
+ *
+ * @version   0.1
+ * @date      Sun, 02 Apr 2006 08:07:46 -0200
+ *
+ * @brief     Defines a ac_tlm memory.
+ *
+ * @attention Copyright (C) 2002-2005 --- The ArchC Team
+ *
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) any later version.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Lesser General Public License for more details.
+ *
+ *
+ */
+
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef AC_TLM_PERIPHERAL_H_
+#define AC_TLM_PERIPHERAL_H_
+
+//////////////////////////////////////////////////////////////////////////////
+
+// Standard includes
+// SystemC includes
+#include <systemc>
+// ArchC includes
+#include "ac_tlm_protocol.H"
+
+//////////////////////////////////////////////////////////////////////////////
+
+// using statements
+using tlm::tlm_transport_if;
+
+//////////////////////////////////////////////////////////////////////////////
+
+//#define DEBUG
+
+// TODO VER TAMANHO E DAR OS ENDERECOS
+
+// Variables addresses
+#define Z1_ADDR = 0x6400000;    // complex 1
+#define Z2_ADDR = 0x6400000;    // complex 2
+#define D1_ADDR = 0x6400000;    // double 1
+#define D2_ADDR = 0x6400000;    // double 2
+
+// Result addresses
+#define C_ADD_ADDR = 0x6400000; // z1+z2
+#define C_SUB_ADDR = 0x6400000; // z1-z2
+#define C_MULT_ADDR = 0x6400000;    //z1*z2
+#define C_MOD_ADDR = 0x6400000; // |z1|
+#define C_SCALAR_ADDR = 0x6400000; // z.r *= d1; + z.i *= d2;
+#define D_ADD_ADDR = 0x6400000; // d1+d2
+#define D_SUB_ADDR = 0x6400000; // d1-d2
+#define D_MULT_ADDR = 0x6400000;    //d1*d2
+#define D_LOG_ADDR = 0x6400000; //log d1
+#define D_FMOD1_ADDR = 0x6400000; //d1%1.0
+#define D_FLOOR_ADDR = 0x6400000; //floor(d1)
+
+
+
+/// Namespace to isolate memory from ArchC
+namespace user
+{
+
+/// A TLM memory
+class ac_tlm_peripheral:
+  public sc_module,
+  public ac_tlm_transport_if // Using ArchC TLM protocol
+{
+private:
+    typedef struct {
+        double r;
+        double i;
+    } Complex;
+
+    double d1, d2;
+    Complex c1, c2;
+    
+    void do_op();
+    
+public:
+  /// Exposed port with ArchC interface
+  sc_export< ac_tlm_transport_if > target_export;
+
+  SC_HAS_PROCESS(ac_tlm_peripheral);
+
+  /// Internal write
+  ac_tlm_rsp_status writem( const uint32_t & , const uint32_t & );
+  /// Internal read
+  ac_tlm_rsp_status readm( const uint32_t & , uint32_t & );
+
+  void send_int();
+
+  /**
+   * Implementation of TLM transport method that
+   * handle packets of the protocol doing apropriate actions.
+   * This method must be implemented (required by SystemC TLM).
+   * @param request is a received request packet
+   * @return A response packet to be send
+  */
+  ac_tlm_rsp transport( const ac_tlm_req &request ) {
+
+    ac_tlm_rsp response;
+
+    switch( request.type ) {
+    case READ :     // Packet is a READ one
+      #ifdef DEBUG  // Turn it on to print transport level messages
+    cout << "Transport READ at 0x" << hex << request.addr << " value ";
+    cout << response.data << endl;
+      #endif
+      response.status = readm( request.addr , response.data );
+      break;
+    case WRITE:     // Packet is a WRITE
+      #ifdef DEBUG
+    cout << "Transport WRITE at 0x" << hex << request.addr << " value ";
+    cout << request.data << endl;
+      #endif
+      response.status = writem( request.addr , request.data );
+      break;
+    default :
+      response.status = ERROR;
+      break;
+    }
+
+    return response;
+  }
+
+
+  /**
+   * Default constructor.
+   *
+   * @param k Memory size in kilowords.
+   *
+   */
+  ac_tlm_peripheral( sc_module_name module_name , int k = 1024 );
+
+  /**
+   * Default destructor.
+   */
+  ~ac_tlm_peripheral();
+
+};
+
+};
+
+#endif //AC_TLM_PERIPHERAL_H_
